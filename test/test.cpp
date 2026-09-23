02e84bc7614899e1d678d3e3da5444629abc9c0e
dsokol101 <dsokol@brooklyn.cuny.edu>
2026-09-23 10:27:56 -0400

    Add swap and print tests

--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <cstdio>
+#include <fstream>
+
 #include "../include/doctest.h"
 #include "../include/lab2.h"
 
@@ -19,4 +22,38 @@ TEST_CASE("Testing sort function") {
     CHECK(a[2] == 3.3);
     CHECK(a[3] == 4.1);
     CHECK(a[4] == 10);
-}   
+}
+
+TEST_CASE("Testing swap function") {
+    double first = 1.5;
+    double second = -2.75;
+
+    swap(first, second);
+
+    CHECK(first == -2.75);
+    CHECK(second == 1.5);
+}
+
+TEST_CASE("Testing print function") {
+    const char* filename = "test_output.txt";
+    double values[3] = {1.5, -2.75, 10};
+
+    print(values, 3, filename);
+
+    std::ifstream output(filename);
+    REQUIRE(output.is_open());
+
+    double value = 0;
+    bool read = static_cast<bool>(output >> value);
+    REQUIRE(read);
+    CHECK(value == 1.5);
+    read = static_cast<bool>(output >> value);
+    REQUIRE(read);
+    CHECK(value == -2.75);
+    read = static_cast<bool>(output >> value);
+    REQUIRE(read);
+    CHECK(value == 10);
+
+    output.close();
+    std::remove(filename);
+}
