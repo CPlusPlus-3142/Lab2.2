834b405f3df24192baaa6cef7ca419b037f3fc1c
dsokol101 <dsokol@brooklyn.cuny.edu>
2026-09-23 12:15:31 -0400

    added include guard

--- include/lab2.h
+++ include/lab2.h
@@ -1,7 +1,13 @@
 #include <cstddef>
 #include <string>
 
+// include guard
+#ifndef LAB2_H
+#define LAB2_H
+
 void print(double arr[], std::size_t size, std::string filename);
 int load(double arr[], std::size_t size, std::string filename);
 void sort(double arr[], std::size_t size);
 void swap(double& x, double& y);
+
+#endif // LAB2_H
